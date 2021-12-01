7d187003f46e295eee464ed446faaa24b29bdc0c
Greg Nicol <linius@gregnicol.uk>
2021-12-01 17:37:07 +0000

    refactor: Tweak for legibility

--- day-01/part2.py
+++ day-01/part2.py
@@ -1,8 +1,6 @@
 with open('data.txt') as f:
-    data = f.readlines()
-
-data = [int(i.rstrip()) for i in data]
+    data = [int(i) for i in f.readlines()]
 
 grouped = [(aa + bb + cc) for aa, bb, cc in zip(data, data[1:], data[2:])]
-part2 = sum([int(second > first) for first, second in zip(grouped, grouped[1:])])
+part2 = sum([1 for first, second in zip(grouped, grouped[1:]) if second > first])
 print(f"Grouped count = {part2}")
